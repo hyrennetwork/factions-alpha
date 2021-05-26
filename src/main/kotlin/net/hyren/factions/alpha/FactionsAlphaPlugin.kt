9bca2d7b9217a6d551fced9744d6fc1dea6f8989
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-05-26 18:44:56 -0300

    dev

--- src/main/kotlin/net/hyren/factions/alpha/FactionsAlphaPlugin.kt
+++ src/main/kotlin/net/hyren/factions/alpha/FactionsAlphaPlugin.kt
@@ -1,17 +1,26 @@
 package net.hyren.factions.alpha
 
+import net.hyren.core.shared.CoreProvider
+import net.hyren.core.shared.applications.status.ApplicationStatus
+import net.hyren.core.shared.applications.status.task.ApplicationStatusTask
+import net.hyren.core.shared.scheduler.AsyncScheduler
 import net.hyren.core.spigot.command.registry.CommandRegistry
 import net.hyren.core.spigot.misc.plugin.CustomPlugin
 import net.hyren.factions.alpha.commands.GameModeCommand
 import org.bukkit.Bukkit
+import java.util.concurrent.TimeUnit
 
 /**
  * @author Gutyerrez
  */
 class FactionsAlphaPlugin : CustomPlugin() {
 
+    private var onlineSince = 0L
+
     override fun onEnable() {
         FactionsAlphaProvider.prepare()
+
+        onlineSince = System.currentTimeMillis()
 
         /**
          * Commands
@@ -47,6 +56,37 @@ class FactionsAlphaPlugin : CustomPlugin() {
 
             it.time = 1200
         }
+
+        /**
+         * Application Status
+         */
+
+        AsyncScheduler.scheduleAsyncRepeatingTask(
+            object : ApplicationStatusTask(
+                ApplicationStatus(
+                    CoreProvider.application.name,
+                    CoreProvider.application.applicationType,
+                    CoreProvider.application.server,
+                    CoreProvider.application.address,
+                    onlineSince
+                )
+            ) {
+                override fun buildApplicationStatus(
+                    applicationStatus: ApplicationStatus
+                ) {
+                    val runtime = Runtime.getRuntime()
+
+                    applicationStatus.heapSize = runtime.totalMemory()
+                    applicationStatus.heapMaxSize = runtime.maxMemory()
+                    applicationStatus.heapFreeSize = runtime.freeMemory()
+
+                    applicationStatus.onlinePlayers = Bukkit.getOnlinePlayers().size
+                }
+            },
+            0,
+            1,
+            TimeUnit.SECONDS
+        )
     }
 
 }
